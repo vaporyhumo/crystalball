59a888f998fcbb53699545b4729733018536e93a
Rodrigo Andrés Contreras Vilina <roanvilina@gmail.com>
2021-03-22 01:28:31 -0300

    WIP

--- lib/crystalball/map_generator/helpers/path_filter.rb
+++ lib/crystalball/map_generator/helpers/path_filter.rb
@@ -5,11 +5,14 @@ module Crystalball
     module Helpers
       # Helper module to filter file paths
       module PathFilter
-        attr_reader :root_path
+        attr_reader :root_path, :exclude_sources
+
 
         # @param [String] root_path - absolute path to root folder of repository
-        def initialize(root_path = Dir.pwd)
+        # @param [Array<String>] exlcude_sources - TODO: add comment 
+        def initialize(root_path = Dir.pwd, exclude_sources: [%r{vendor/}])
           @root_path = root_path
+          @exclude_sources = exclude_sources
         end
 
         # @param [Array<String>] paths
@@ -17,7 +20,14 @@ module Crystalball
         def filter(paths)
           paths
             .select { |file_name| file_name.start_with?(root_path) }
-            .map { |file_name| file_name.sub("#{root_path}/", '') }
+            .map    { |file_name| file_name.sub("#{root_path}/", '') }
+            .reject { |file_name| matches_exclude_source?(file_name) }
+        end
+
+        # @param [String] file path
+        # @return - TODO: add comment
+        def matches_exclude_source?(file_name)
+          exclude_sources.any? { |exclude_source| exclude_source.match?(file_name) }
         end
       end
     end
--- spec/map_generator/helpers/path_filter_spec.rb
+++ spec/map_generator/helpers/path_filter_spec.rb
@@ -3,15 +3,30 @@
 require 'spec_helper'
 
 describe Crystalball::MapGenerator::Helpers::PathFilter do
-  subject(:helper) { Class.new.tap { |c| c.include described_class }.new(root) }
-  let(:root) { '/foo' }
-  let(:paths) { ['/foo/file.rb', '/abc/file1.rb'] }
-
   describe '#filter' do
     subject { helper.filter(paths) }
 
-    it 'takes paths relative too root only' do
-      is_expected.to eq(%w[file.rb])
+    context 'when no exclude source is given' do
+      let(:helper) { Class.new.tap { |c| c.include described_class }.new(root) }
+      let(:root) { '/foo' }
+      let(:paths) { ['/foo/file.rb', '/abc/file1.rb'] }
+
+      it 'takes paths relative too root only' do
+        is_expected.to eq(%w[file.rb])
+      end
+    end
+
+    context 'when given a source to exlcude' do
+      let(:helper) do
+        Class.new.tap { |c| c.include described_class }.new(root, exclude_sources: exclude_sources)
+      end
+      let(:root) { '/foo' }
+      let(:exclude_sources) { [%r{vendor/}] }
+      let(:paths) { ['/foo/file.rb', '/foo/vendor/file.rb'] }
+
+      it 'excludes given sources' do
+        is_expected.to eq(%w[file.rb])
+      end
     end
   end
 end
